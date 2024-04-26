0321534f6348f0ebf56ebf09810516689d375ffe
Moldir <51477205+Moldir99@users.noreply.github.com>
2024-04-26 18:55:42 +0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+function Reset() {
+  document.getElementById("form").reset();
+}
+
+function mySubmit() {
+  document.getElementById("form").submit();
+  window.open("https://www.vilniustech.lt");
+}
+
+
